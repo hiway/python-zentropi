782fa5e1726202720a40b9aec553c816b017afe9
Harshad Sharma <harshad@sharma.io>
2017-04-06 06:46:44 +0530

    Mark __main__.py as uncovered for now.

--- src/zentropi/__main__.py
+++ src/zentropi/__main__.py
@@ -8,8 +8,8 @@ Why does this file exist, and why __main__? For more info, read:
 - https://www.python.org/dev/peps/pep-0338/
 - https://docs.python.org/2/using/cmdline.html#cmdoption-m
 - https://docs.python.org/3/using/cmdline.html#cmdoption-m
-"""
-from zentropi.cli import main
+"""  # pragma: no cover
+from zentropi.cli import main  # pragma: no cover
 
-if __name__ == "__main__":
+if __name__ == "__main__":  # pragma: no cover
     main()
