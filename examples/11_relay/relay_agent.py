7498d01e4e8d28910e8295b38b30d07c611cc8d0
Harshad Sharma <harshad@sharma.io>
2017-04-21 07:03:08 +0530

    Add example relay agent - does not do actual GPIO yet; but can work with telegram bot.

---
+++ examples/11_relay/relay_agent.py
@@ -0,0 +1,35 @@
+# coding=utf-8
+from zentropi import Agent
+from zentropi import on_event
+from zentropi import on_message
+from zentropi import on_state
+
+
+class Relay(Agent):
+    @on_event('*** started')
+    def setup(self, event):
+        # Define custom states.
+        self.states.power = False
+
+    @on_message('switch {power}', parse=True)
+    def switch(self, message):
+        power = message.data.power.lower().strip()  # Clean input
+        if power in ['on', 'up']:
+            self.states.power = True  # Will trigger @on_state('power')
+        elif power in ['off', 'down']:
+            self.states.power = False  # Will trigger @on_state('power')
+        else:
+            return 'I did not understand {!r}; try "switch [on|off]".'.format(power)
+        return 'Switching power {}'.format(power)  # Send reply.
+
+    @on_state('power')
+    def on_test_state(self, state):
+        print('Change GPIO value to', state.data.value)
+        return True  # Accept state change
+
+
+if __name__ == '__main__':
+    agent = Relay('Relay')
+    agent.connect('redis://localhost:6379')
+    agent.join('telegram')
+    agent.run()
