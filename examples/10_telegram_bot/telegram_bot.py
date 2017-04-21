c2042ad8cb265caea342a5117d361c3849fcbfec
Harshad Sharma <harshad@sharma.io>
2017-04-21 07:02:39 +0530

    Update example telegram agent.

--- examples/10_telegram_bot/telegram_bot.py
+++ examples/10_telegram_bot/telegram_bot.py
@@ -1,7 +1,6 @@
 # coding=utf-8
 import traceback
 
-import asyncio
 import os
 from aiotg import Bot  # pip install aiotg
 from zentropi import Agent  # pip install zentropi
@@ -13,7 +12,7 @@ TELEGRAM_BOT_API_TOKEN = os.getenv('TELEGRAM_BOT_API_TOKEN', None)
 assert TELEGRAM_BOT_API_TOKEN
 
 
-class TelegramAgent(Agent):
+class SimpleTelegramAgent(Agent):
     def __init__(self, name=None):
         super().__init__(name=name)
         self._bot = Bot(api_token=TELEGRAM_BOT_API_TOKEN,
@@ -38,9 +37,8 @@ class TelegramAgent(Agent):
             })
             self.sent_telegram_messages.update({sent_msg.id: message['chat']['id']})
         except Exception:
-            print(message)
             print(traceback.format_exc())
-            self.emit('telegram-error', data={'traceback': traceback.format_exc()})
+            print(message.name, message.data)
 
     @on_message('*')
     async def telegram_message_reply(self, message):
@@ -51,22 +49,11 @@ class TelegramAgent(Agent):
             del self.sent_telegram_messages[message.reply_to]
             await self._bot.send_message(chat_id=chat_id, text=message.name)
         except Exception:
-            raise
-
-    @on_event('telegram-reply')
-    async def on_telegram_reply(self, event):
-        if event.reply_to not in self.sent_telegram_messages:
-            return
-        try:
-            chat_id = self.sent_telegram_messages[event.reply_to]
-            del self.sent_telegram_messages[event.reply_to]
-            await self._bot.send_message(chat_id=chat_id, text=event.data.text)
-        except Exception:
-            self.emit('telegram-error', data={'traceback': traceback.format_exc()})
+            print(traceback.format_exc())
+            print(message.name, message.data)
 
     @on_event('*** started')
     def on_started(self, event):
-        print('*** starting telegram bot')
         self.spawn(self._bot.loop())
 
     @on_event('*** stopped')
@@ -76,7 +63,7 @@ class TelegramAgent(Agent):
 
 if __name__ == '__main__':
     endpoint = 'redis://localhost:6379'
-    telegram = TelegramAgent('telegram')
-    telegram.connect(endpoint)
-    telegram.join('telegram')
-    telegram.run()
+    agent = SimpleTelegramAgent('telegram')
+    agent.connect(endpoint)
+    agent.join('telegram')
+    agent.run()
