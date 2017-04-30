455b038a3e4e289c9c286226c6675318261f3f95
Harshad Sharma <harshad@sharma.io>
2017-04-30 11:29:04 +0530

    Simplify example telegram bot; make an EchoBot using extras => TelegramBot.

--- examples/10_telegram_bot/telegram_bot.py
+++ examples/10_telegram_bot/telegram_bot.py
@@ -1,71 +1,30 @@
 # coding=utf-8
-import traceback
-
-import os
-from aiotg import Bot  # pip install aiotg
-from zentropi import Agent  # pip install zentropi
-from zentropi import on_event
-from zentropi import on_message
-
-TELEGRAM_BOT_NAME = os.getenv('TELEGRAM_BOT_NAME', 'ExampleBot')
-TELEGRAM_BOT_API_TOKEN = os.getenv('TELEGRAM_BOT_API_TOKEN', None)
-assert TELEGRAM_BOT_API_TOKEN
+from zentropi import (
+    Agent,
+    on_message,
+    run_agents,
+    ZentropiShell,
+)
+from zentropi.extra.agents import TelegramAgent
 
 
-class SimpleTelegramAgent(Agent):
-    def __init__(self, name=None):
-        super().__init__(name=name)
-        self._bot = Bot(api_token=TELEGRAM_BOT_API_TOKEN,
-                        name=TELEGRAM_BOT_NAME,
-                        api_timeout=10)
-        self._bot.default(self.on_incoming_telegram_message)
-        self.sent_telegram_messages = {}
-
-    def on_incoming_telegram_message(self, chat, message):
-        try:
-            text = message['text']
-            if len(text) > 128:
-                text = text[:127] + '…'
-            name = '{} {}'.format(message['from']['first_name'], message['from']['last_name'])
-            sent_msg = self.message(text, data={
-                'text': text,
-                'message_id': message['message_id'],
-                'chat_id': message['chat']['id'],
-                'user_id': message['from']['id'],
-                'username': message['from']['username'],
-                'name': name,
-            })
-            print('>>>', text)
-            self.sent_telegram_messages.update({sent_msg.id: message['chat']['id']})
-        except Exception:
-            print(traceback.format_exc())
-            print(message.name, message.data)
+class EchoBot(Agent):
+    @on_message('help', fuzzy=True)
+    async def help(self, message):
+        return 'Hi, I am {}. I echo messages.'.format(self.name)
 
     @on_message('*')
-    async def telegram_message_reply(self, message):
-        if message.reply_to not in self.sent_telegram_messages:
+    async def echo(self, message):
+        # This handler will be called for all messages: @on_message('*'),
+        # we want to skip messages sent by our own agent ("Echo: ...")
+        if message.source == self.name:
+            # quit here if we are the source of the message.
             return
-        try:
-            chat_id = self.sent_telegram_messages[message.reply_to]
-            del self.sent_telegram_messages[message.reply_to]
-            await self._bot.send_message(chat_id=chat_id, text=message.name)
-            print('<<<', message.name)
-        except Exception:
-            print(traceback.format_exc())
-            print(message.name, message.data)
-
-    @on_event('*** started')
-    def on_started(self, event):
-        self.spawn(self._bot.loop())
-
-    @on_event('*** stopped')
-    def on_stopped(self, event):
-        self._bot.stop()
+        return 'Echo: {}'.format(message.data.text)
 
 
 if __name__ == '__main__':
-    endpoint = 'redis://localhost:6379'
-    agent = SimpleTelegramAgent('telegram')
-    agent.connect(endpoint)
-    agent.join('telegram')
-    agent.run()
+    echo_bot = EchoBot('echo_bot')
+    telegram_agent = TelegramAgent(name='tg')
+    shell = ZentropiShell(name='shell')
+    run_agents(echo_bot, telegram_agent, shell)
