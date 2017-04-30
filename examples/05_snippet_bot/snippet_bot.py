3a78224d2c0e3fd5f61eaf1b6917553e5de3d2dd
Harshad Sharma <harshad@sharma.io>
2017-04-30 11:57:06 +0530

    Update examples.

---
+++ examples/05_snippet_bot/snippet_bot.py
@@ -0,0 +1,72 @@
+# coding=utf-8
+from zentropi import (
+    Agent,
+    on_event,
+    on_message,
+    run_agents,
+    ZentropiShell,
+)
+
+
+class SnippetBot(Agent):
+    """
+    An agent that can save and search text snippets.
+    """
+    def save(self, text: str) -> None:
+        """Saves text to data-store."""
+        print('DUMMY Save: {!r}'.format(text))
+
+    def search(self, query: str) -> list:
+        """Searches data-store for query, returns list of matching strings."""
+        print('DUMMY Search: {!r}'.format(query))
+        return []
+
+    def delete(self, id: str) -> None:
+        print('DUMMY Delete: {!r}'.format(id))
+
+    @on_event('snippet-save')
+    def on_snippet_save(self, event):
+        text = event.data.text
+        if not text:
+            self.emit('snippet-save-error', data={'error': '`data.text` not found in event-id: {}'.format(event.id)})
+            return
+        self.save(text)
+        self.emit('snippet-save-complete')
+
+    @on_event('snippet-search')
+    def on_snippet_search(self, event):
+        query = event.data.query
+        if not query:
+            self.emit('snippet-query-error', data={'error': '`data.query` not found in event-id: {}'.format(event.id)})
+            return
+        results = self.search(query)
+        self.emit('snippet-query-results', data={'results': results})
+
+    @on_event('snippet-delete')
+    def on_snippet_delete(self, event):
+        id_ = event.data.id
+        if not id_:
+            self.emit('snippet-delete-error', data={'error': '`data.id` not found in event-id: {}'.format(event.id)})
+            return
+        self.delete(id_)
+        self.emit('snippet-delete-complete')
+
+    @on_message('snip {snippet}', parse=True)
+    def on_save(self, message):
+        snippet = message.data.snippet
+        self.save(snippet)
+        return 'Saved'
+
+    @on_message('snips {query}', parse=True)
+    def on_search(self, message):
+        query = message.data.query.strip().lower()
+        results = self.search(query)
+        if not results:
+            return 'Found nothing.'
+        return '\n'.join(results)
+
+
+if __name__ == '__main__':
+    snippet_bot = SnippetBot(name='snip')
+    shell = ZentropiShell(name='shell')
+    run_agents(snippet_bot, shell)
