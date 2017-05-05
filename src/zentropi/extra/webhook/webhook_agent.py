079d00a1cfc45204b4d7c3f49014d50025379a57
Harshad Sharma <harshad@sharma.io>
2017-05-06 00:19:14 +0530

    Use `~/.zentropi/webhook.crt` and `~/.zentropi/webhook.key` to initialize SSL.

--- src/zentropi/extra/webhook/webhook_agent.py
+++ src/zentropi/extra/webhook/webhook_agent.py
@@ -4,13 +4,34 @@
 # coding=utf-8
 
 import asyncio
+import ssl
+
 import os
 from aiohttp import web
 from zentropi import Agent
 
-
+RELATIVE_BASE_DIR = '~/.zentropi/'
+BASE_DIR = os.path.abspath(os.path.expanduser(RELATIVE_BASE_DIR))
 TOKEN = os.getenv('ZENTROPI_WEBHOOK_TOKEN', None)
-assert TOKEN, 'Error: export ZENTROPI_WEBHOOK_TOKEN="[32-or-more-random-characters]" and pass as ?token="" in request.'
+assert TOKEN, 'Error: export ZENTROPI_WEBHOOK_TOKEN="[32-or-more-urlsafe-random-characters]" ' \
+              'Send the token with client-request as /emit?token=[32-or-more-urlsafe-random-characters].'
+
+
+def get_ssl_context():
+    cert_path = os.path.join(BASE_DIR, 'webhook.crt')
+    key_path = os.path.join(BASE_DIR, 'webhook.key')
+    if not os.path.exists(cert_path):
+        raise ValueError('Certificate file does not exist: {cert_path}'.format(cert_path=cert_path))
+    if not os.path.exists(key_path):
+        raise ValueError('Key file does not exist: {key_path}'.format(key_path=key_path))
+    ssl_context = ssl.SSLContext(ssl.PROTOCOL_SSLv23)
+    try:
+        ssl_context.load_cert_chain(cert_path, key_path)
+    except Exception as e:
+        import traceback
+        traceback.print_stack()
+        traceback.print_exc()
+    return ssl_context
 
 
 def web_server(emit_callback):
@@ -19,7 +40,7 @@ def web_server(emit_callback):
     app.router.add_get('/emit', emit_callback)
     host = '127.0.0.1'
     port = 26514
-    web.run_app(app, host=host, port=port, loop=loop)
+    web.run_app(app, host=host, port=port, loop=loop, ssl_context=get_ssl_context())
 
 
 class WebhookAgent(Agent):
