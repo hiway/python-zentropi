5c42e374e03e6dca98a2a2e996fbc6e1a7d9bb5e
Harshad Sharma <harshad@sharma.io>
2017-10-29 22:06:57 +0530

    Move command() and @on_command() from uzentropi.

--- src/zentropi/agent.py
+++ src/zentropi/agent.py
@@ -1,5 +1,22 @@
-from uzentropi import Agent as uAgent
+from uzentropi import Agent as uAgent, RATE_LIMIT, Kind, Frame, MICROPY, PLATFORM
+from uzentropi.agent import wrap_handler
+from uzentropi.utils import random_id
+
+
+def on_command(_name: str, rate_limit=RATE_LIMIT, **expects):
+    def wrapper(func):
+        return wrap_handler(Kind.COMMAND, _name, func, expects, rate_limit=rate_limit)
+
+    return wrapper
 
 
 class Agent(uAgent):
-    pass
+    async def command(self, _name, space='', broadcast=True, internal=False, **kwargs):
+        frame = Frame(Kind.COMMAND, id=random_id(), name=_name, data=kwargs, meta={'source': self.name, 'space': space})
+        try:
+            return await self.send_frame(frame, broadcast, internal)
+        except Exception as e:
+            if PLATFORM is MICROPY:
+                return
+            import traceback
+            traceback.print_exc()
